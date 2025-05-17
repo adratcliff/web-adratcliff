13e7a3dd2c773fae1ce878e312f674c44a9e54ee
Alexander Ratcliff <alexanderratcliff99@gmail.com>
2025-05-17 13:37:22 +1000

    vuetify component styling defaults

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,28 @@
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/styles';
 
 // Vuetify
-import { createVuetify } from 'vuetify'
+import { createVuetify } from 'vuetify';
 
-export default createVuetify(
+export default createVuetify({
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+  defaults: {
+    VChip: {
+      variant: 'flat',
+      size: 'small',
+    },
+    VTextField: {
+      variant: 'outlined',
+      density: 'compact',
+    },
+    VTextarea: {
+      variant: 'outlined',
+      density: 'compact',
+    },
+    VCombobox: {
+      variant: 'outlined',
+      density: 'compact',
+    },
+  },
+});
